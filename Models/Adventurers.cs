d895b8e2c00d92e4f475ac381626912578206870
WenZhu72 <Wenzhezhu2005@gmail.com>
2026-06-23 15:33:40 +0100

    Initial ASP.NET Core Web API setup

---
+++ Models/Adventurers.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventureGuildApi.Models
+{
+    public class Adventurers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Level { get; set; }
+        public string GuildRank { get; set; } = string.Empty;
+        public int Gold { get; set; }
+        public int Experience { get; set; }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/guild", () =>
+{
+    var guildInfo = new
+    {
+        Name = "Adventure Guild",
+        Location = "Ironhaven",
+        RankSystem = "Bronze to Mythril"
+    };
+
+    return Results.Ok(guildInfo);
+}).WithName("GetGuildInfo");
+
+app.Run();
