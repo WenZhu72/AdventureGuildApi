72281c5fe2b198e9c0fdd4004aeedd9854f58cd6
WenZhu72 <Wenzhezhu2005@gmail.com>
2026-06-24 13:02:29 +0100

    Better naming convention, refractions and modification of adventurer

--- Program.cs
+++ Program.cs
@@ -11,14 +11,13 @@ var adventurers = new List<Adventurer>
 {
     new Adventurer
     {
-        Id= 1,
+        Id = 1,
         Name = "Aria Stormblade",
         Level = 5,
         GuildRank = "Bronze",
         Gold = 120,
-        Experience = 450,
+        Experience = 450
     },
-
     new Adventurer
     {
         Id = 2,
@@ -48,24 +47,57 @@ app.MapGet("/guild", () =>
     };
 
     return Results.Ok(guildInfo);
-}).WithName("GetGuildInfo");
+})
+.WithName("GetGuildInfo");
 
 app.MapGet("/adventurers", () =>
 {
     return Results.Ok(adventurers);
-}).WithName("GetAdventurers");
-
+})
+.WithName("GetAdventurers");
 
 app.MapGet("/adventurers/{id}", (int id) =>
 {
-    var adventurer = adventurers.FirstOrDefault(a => a.Id == id);
+    var foundAdventurer =
+        adventurers.FirstOrDefault(adventurer => adventurer.Id == id);
 
-    if (adventurer == null)
+    if (foundAdventurer is null)
     {
         return Results.NotFound();
     }
 
-    return Results.Ok(adventurer);
-}).WithName("GetAdventurerById");
+    return Results.Ok(foundAdventurer);
+})
+.WithName("GetAdventurerById");
+
+app.MapPost("/adventurers", (Adventurer newAdventurer) =>
+{
+    newAdventurer.Id = adventurers.Max(adventurer => adventurer.Id) + 1;
+
+    adventurers.Add(newAdventurer);
+
+    return Results.Created($"/adventurers/{newAdventurer.Id}", newAdventurer);
+})
+.WithName("CreateAdventurer");
+
+app.MapPut("/adventurers/{id}", (int id, Adventurer updatedAdventurer) =>
+{
+    var existingAdventurer =
+        adventurers.FirstOrDefault(adventurer => adventurer.Id == id);
+
+    if (existingAdventurer is null)
+    {
+        return Results.NotFound();
+    }
+
+    existingAdventurer.Name = updatedAdventurer.Name;
+    existingAdventurer.Level = updatedAdventurer.Level;
+    existingAdventurer.GuildRank = updatedAdventurer.GuildRank;
+    existingAdventurer.Gold = updatedAdventurer.Gold;
+    existingAdventurer.Experience = updatedAdventurer.Experience;
+
+    return Results.Ok(existingAdventurer);
+})
+.WithName("UpdateAdventurer");
 
 app.Run();
